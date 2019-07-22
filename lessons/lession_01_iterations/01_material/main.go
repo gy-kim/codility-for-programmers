52b7b7d942499b39384795ab5c4581f54702d7bc
Geunyoung Kim <kgysmart@gmail.com>
2019-07-22 22:23:08 +0200

    Add lession 01 iterations (material)

---
+++ lessons/lession_01_iterations/01_material/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func fnFactorial(n int) {
+	factorial := 1
+	for i := 1; i <= n; i++ {
+		fmt.Print(i, " ")
+		factorial *= i
+	}
+	fmt.Println("\nfactorial:", factorial)
+}
+
+func fnAsterisks(n int) {
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Print("* ")
+		}
+		fmt.Println()
+	}
+}
+
+func fnAsterisks2(n int) {
+	for i := n; i > 0; i-- {
+		for j := 0; j < n-i; j++ {
+			fmt.Print(" ")
+		}
+		for j := 0; j < 2*i-1; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+}
+
+func main() {
+	// fnFactorial(5)
+	// fnAsterisks(5)
+	fnAsterisks2(4)
+}
