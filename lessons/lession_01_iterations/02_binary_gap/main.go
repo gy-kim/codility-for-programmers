9ee60c6533e3eaa5990784dfe413429a6a98cda3
Geunyoung Kim <kgysmart@gmail.com>
2019-07-23 09:43:05 +0200

    Add binary

--- lessons/lession_01_iterations/02_binary_gap/main.go
+++ lessons/lession_01_iterations/02_binary_gap/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
 
+	i := int64(9)
+	str := fmt.Sprint(strconv.FormatInt(i, 2))
+	fmt.Println(str)
+	for _, s := range str {
+		fmt.Println(string(s))
+	}
 }
