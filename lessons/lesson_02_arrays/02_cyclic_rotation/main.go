b60dd741eb91e4c0b9e55a2cca40dbe7e736c40a
Geunyoung Kim <kgysmart@gmail.com>
2019-07-24 15:59:29 +0200

    Add cyclic roration

--- lessons/lesson_02_arrays/02_cyclic_rotation/main.go
+++ lessons/lesson_02_arrays/02_cyclic_rotation/main.go
@@ -4,8 +4,7 @@ import "fmt"
 
 func main() {
 	A := []int{3, 8, 9, 7, 6}
-	// {6,3,8,9,7}
-	K := 6
+	K := 7
 
 	result := Solution(A, K)
 	fmt.Println("result:", result)
@@ -14,11 +13,10 @@ func Solution(A []int, K int) []int {
 	if len(A) == 0 || K == 0 || len(A) == K {
 		return A
 	}
-	if len(A) > K {
-		div := len(A) % K
-		return append(A[div:], A[:div]...)
-	} else {
-		div := K % len(A)
-		return append(A[len(A)-div:], A[:len(A)-div]...)
+
+	if len(A) < K {
+		K = K % len(A)
 	}
+
+	return append(A[len(A)-K:], A[:len(A)-K]...)
 }
