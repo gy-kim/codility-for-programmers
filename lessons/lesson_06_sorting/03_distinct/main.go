35a5d10d49d7445d10b9a9d434fcbef5c4d90bfb
Geunyoung Kim <kgysmart@gmail.com>
2019-07-31 09:14:11 +0200

    Add sorting (distinct)

---
+++ lessons/lesson_06_sorting/03_distinct/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// Write a function
+
+// class Solution { public int solution(int[] A); }
+
+// that, given an array A consisting of N integers, returns the number of distinct values in array A.
+
+// For example, given array A consisting of six elements such that:
+
+//  A[0] = 2    A[1] = 1    A[2] = 1
+//  A[3] = 2    A[4] = 3    A[5] = 1
+// the function should return 3, because there are 3 distinct values appearing in array A, namely 1, 2 and 3.
+
+// Write an efficient algorithm for the following assumptions:
+
+// N is an integer within the range [0..100,000];
+// each element of array A is an integer within the range [−1,000,000..1,000,000].
+
+func main() {
+	A := []int{2, 1, 1, 2, 3, 1}
+
+	result := Solution(A)
+
+	fmt.Println("result:", result)
+
+}
+
+func Solution(A []int) int {
+
+	if len(A) == 0 {
+		return 0
+	}
+
+	sort.Ints(A)
+	result := 1
+	for i := 1; i < len(A); i++ {
+		if A[i] != A[i-1] {
+			result++
+		}
+	}
+	return result
+}
